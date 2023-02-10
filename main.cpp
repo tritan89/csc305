7f60ac3ecbefe9ea941f058db21bd697cc9f241c
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-02-09 19:25:36 -0800

    trying parllelogram

--- main.cpp
+++ main.cpp
@@ -1,100 +1,350 @@
-////////////////////////////////////////////////////////////////////////////////
-#include <algorithm>
-#include <complex>
-#include <fstream>
+// C++ include
 #include <iostream>
-#include <numeric>
-#include <vector>
 #include <string>
-////////////////////////////////////////////////////////////////////////////////
+#include <vector>
 
-typedef std::complex<double> Point;
-typedef std::vector<Point> Polygon;
+// Utilities for the Assignment
+#include "utils.h"
 
-double inline det(const Point &u, const Point &v) {
-	Point p1 = u;
-	Point p2 = v; 
-	double detr = p1.real() * p2.real() - p1.imag()*p2.imag();
+// Image writing library
+#define STB_IMAGE_WRITE_IMPLEMENTATION // Do not include this line twice in your project!
+#include "stb_image_write.h"
 
-	return detr;
-}
+// Shortcut to avoid Eigen:: everywhere, DO NOT USE IN .h
+using namespace Eigen;
 
-struct Compare {
-	Point p0; // Leftmost point of the poly
-	bool operator ()(const Point &p1, const Point &p2) {
-		// TODO
-		return true;
-	}
-};
+void raytrace_sphere()
+{
+    std::cout << "Simple ray tracer, one sphere with orthographic projection" << std::endl;
 
-bool inline salientAngle(Point &a, Point &b, Point &c) {
-	// TODO
-	return false;
-}
+    const std::string filename("sphere_orthographic.png");
+    MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
+    MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
 
-////////////////////////////////////////////////////////////////////////////////
+    const Vector3d camera_origin(0, 0, 3);
+    const Vector3d camera_view_direction(0, 0, -1);
 
-Polygon convex_hull(std::vector<Point> &points) {
-	Compare order;
-	// TODO
-	order.p0 = Point(0, 0);
-	std::sort(points.begin(), points.end(), order);
-	Polygon hull;
-	// TODO
-	// use salientAngle(a, b, c) here
-	return hull;
-}
+    // The camera is orthographic, pointing in the direction -z and covering the
+    // unit square (-1,1) in x and y
+    const Vector3d image_origin(-1, 1, 1);
+    const Vector3d x_displacement(2.0 / C.cols(), 0, 0);
+    const Vector3d y_displacement(0, -2.0 / C.rows(), 0);
 
-////////////////////////////////////////////////////////////////////////////////
+    // Single light source
+    const Vector3d light_position(-1, 1, 1);
 
-std::vector<Point> load_xyz(const std::string &filename) {
-	std::vector<Point> points;
-	
-	std::ifstream in(filename);
-	int num;
-	
-	in >> num;
-	
-	
-	
-	float a, b, z;
-	if (in.is_open()) {
-    while (in>> a >> b >> z){
-        Point p1 = Point(a, b);
-        points.push_back(p1);
-		
+    for (unsigned i = 0; i < C.cols(); ++i)
+    {
+        for (unsigned j = 0; j < C.rows(); ++j)
+        {
+            const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
+
+            // Prepare the ray
+            const Vector3d ray_origin = pixel_center;
+            const Vector3d ray_direction = camera_view_direction;
+
+            // Intersect with the sphere
+            // NOTE: this is a special case of a sphere centered in the origin and for orthographic rays aligned with the z axis
+            Vector2d ray_on_xy(ray_origin(0), ray_origin(1));
+            const double sphere_radius = 0.9;
+
+            if (ray_on_xy.norm() < sphere_radius)
+            {
+                // The ray hit the sphere, compute the exact intersection point
+                Vector3d ray_intersection(
+                    ray_on_xy(0), ray_on_xy(1),
+                    sqrt(sphere_radius * sphere_radius - ray_on_xy.squaredNorm()));
+
+                // Compute normal at the intersection point
+                Vector3d ray_normal = ray_intersection.normalized();
+
+                // Simple diffuse model
+                C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
+
+                // Clamp to zero
+                C(i, j) = std::max(C(i, j), 0.);
+
+                // Disable the alpha mask for this pixel
+                A(i, j) = 1;
+            }
+        }
     }
-	}
-	return points;
+
+    // Save to png
+    write_matrix_to_png(C, C, C, A, filename);
 }
 
-void save_obj(const std::string &filename, Polygon &poly) {
-	std::ofstream out(filename);
-	if (!out.is_open()) {
-		throw std::runtime_error("failed to open file " + filename);
-	}
-	out << std::fixed;
-	for (const auto &v : poly) {
-		out << "v " << v.real() << ' ' << v.imag() << " 0\n";
-	}
-	for (size_t i = 0; i < poly.size(); ++i) {
-		out << "l " << i+1 << ' ' << 1+(i+1)%poly.size() << "\n";
-	}
-	out << std::endl;
+void raytrace_perspective()
+{
+    std::cout << "Simple ray tracer, one parallelogram with orthographic projection" << std::endl;
+
+    const std::string filename("plane_perspective.png");
+    MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
+    MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
+
+    const Vector3d camera_origin(0, 0, 3); // eye positon E
+    const Vector3d camera_view_direction(0, 0, -1);
+
+    // The camera is orthographic, pointing in the direction -z and covering the unit square (-1,1) in x and y
+    const Vector3d image_origin(-1, 1, 1);
+    const Vector3d x_displacement(2.0 / C.cols(), 0, 0);
+    const Vector3d y_displacement(0, -2.0 / C.rows(), 0);
+
+    // TODO: Parameters of the parallelogram (position of the lower-left corner + two sides)
+    const Vector3d pgram_origin(-0.5, -0.5, 0); //Xa Ya Za
+    const Vector3d pgram_u(0, 0.7, -10);// a b c
+    const Vector3d pgram_v(1, 0.4, 0); // d e f 
+    const Vector3d norm = pgram_u.cross(pgram_v); // gives us last point
+    
+    double dir = norm.dot(pgram_origin);
+    // intersection variables for plane line intersection
+    // double n_a = norm[0] ;
+    // double n_b = norm[1] ;
+    // double n_c = norm[2] ;
+
+    // double p_x = pgram_origin[0] ;
+    // double p_y = pgram_origin[1] ;
+    // double p_z = pgram_origin[2] ;
+
+
+
+
+    //step 1 take in the parameters of the shape
+    //step 2 get the equation of the ray 
+    //step 3 setup the equality between the ray and the shape
+    // get 
+
+
+
+
+    // Single light source
+    const Vector3d light_position(-1, 1, 1);
+
+    for (unsigned i = 0; i < C.cols(); ++i)
+    {
+        for (unsigned j = 0; j < C.rows(); ++j)
+        {
+            const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
+
+            // Prepare the ray
+            const Vector3d ray_origin = pixel_center; //Xe Ye Ze
+            const Vector3d ray_direction = camera_view_direction; //g h i 
+
+            
+            // //variables for intersection 
+            // double a = pgram_u[0];
+            // double b = pgram_u[1];
+            // double c = pgram_u[2];
+
+            // double d = pgram_v[0];
+            // double e = pgram_v[1];
+            // double f = pgram_v[2];
+
+            // double g = ray_direction[0];
+            // double h = ray_direction[1];
+            // double i = ray_direction[2];
+
+            // double j_ = pgram_origin[0] - ray_origin[0];
+            // double k = pgram_origin[1] - ray_origin[1];
+            // double l = pgram_origin[2] - ray_origin[2];
+
+            // // solve for each varible B Y T 
+
+            // double beta =  (j_*d-g*k)/(a*d-b*c);
+            // double lam = (j_*b-g*l)/(a*e-b*c);
+            // double theta = (k*a-h*j_)/(g*f-e*h);
+            // bool intersect = false;
+            // if (theta >0 && (lam<= 1 && lam>=0) && (beta<= 1 && beta>=0)){
+            //     intersect = true;
+            // }
+
+
+
+
+            // TODO: Check if the ray intersects with the parallelogram
+            
+            if (true)
+            {
+                //variables for line equation
+                // double r_x = ray_origin[0] ;
+                // double r_y = ray_origin[1] ;
+                // double r_z = ray_origin[2] ;
+
+                // double d_x = ray_direction[0];
+                // double d_y = ray_direction[1];
+                // double d_z = ray_direction[2];
+            double intersect_x = (dir - norm.dot(ray_origin))/ norm.dot(ray_direction);
+
+
+
+
+                
+                    
+
+
+                // TODO: The ray hit the parallelogram, compute the exact intersection
+                // point
+                Vector3d ray_intersection = ray_origin + ray_direction.normalized()*intersect_x;
+
+                // TODO: Compute normal at the intersection point
+                Vector3d ray_normal = ray_intersection.normalized();
+
+                // Simple diffuse model
+                C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
+
+                // Clamp to zero
+                C(i, j) = std::max(C(i, j), 0.);
+
+                // Disable the alpha mask for this pixel
+                A(i, j) = 1;
+            }
+        }
+    }
+
+    // Save to png
+    write_matrix_to_png(C, C, C, A, filename);
 }
 
-////////////////////////////////////////////////////////////////////////////////
+void  raytrace_parallelogram()
+{
+    std::cout << "Simple ray tracer, one parallelogram with perspective projection" << std::endl;
 
-int main(int argc, char * argv[]) {
-	
-	if (argc <= 2) {
-		std::cerr << "Usage: " << argv[0] << " points.xyz output.obj" << std::endl;
-	}
-	
-  	std::vector<Point> points = load_xyz(argv[1]);
-	for(int i=0; i < points.size(); i++)
-   		std::cout << points.at(i) << ' ' <<'\n';
-	//Polygon hull = convex_hull(points);
-	//save_obj(argv[2], hull);
-	return 0;
+    const std::string filename("plane_orthographic.png");
+    MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
+    MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
+
+    const Vector3d camera_origin(0, 0, 3);
+    const Vector3d camera_view_direction(0, 0, -1);
+
+    // The camera is perspective, pointing in the direction -z and covering the unit square (-1,1) in x and y
+    const Vector3d image_origin(-1, 1, 1);
+    const Vector3d x_displacement(2.0 / C.cols(), 0, 0);
+    const Vector3d y_displacement(0, -2.0 / C.rows(), 0);
+
+    // TODO: Parameters of the parallelogram (position of the lower-left corner + two sides)
+    const Vector3d pgram_origin(-0.5, -0.5, 0);
+    const Vector3d pgram_u(0, 0.7, -10);
+    const Vector3d pgram_v(1, 0.4, 0);
+
+    // Single light source
+    const Vector3d light_position(-1, 1, 1);
+
+    for (unsigned i = 0; i < C.cols(); ++i)
+    {
+        for (unsigned j = 0; j < C.rows(); ++j)
+        {
+            const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
+
+            // TODO: Prepare the ray (origin point and direction)
+            const Vector3d ray_origin = pixel_center;
+            const Vector3d ray_direction = camera_view_direction;
+
+            // TODO: Check if the ray intersects with the parallelogram
+            if (true)
+            {
+                // TODO: The ray hit the parallelogram, compute the exact intersection
+                // point
+                Vector3d ray_intersection(0, 0, 0);
+
+                // TODO: Compute normal at the intersection point
+                Vector3d ray_normal = ray_intersection.normalized();
+
+                // Simple diffuse model
+                C(i, j) = (light_position - ray_intersection).normalized().transpose() * ray_normal;
+
+                // Clamp to zero
+                C(i, j) = std::max(C(i, j), 0.);
+
+                // Disable the alpha mask for this pixel
+                A(i, j) = 1;
+            }
+        }
+    }
+
+    // Save to png
+    write_matrix_to_png(C, C, C, A, filename);
+}
+
+void raytrace_shading()
+{
+    std::cout << "Simple ray tracer, one sphere with different shading" << std::endl;
+
+    const std::string filename("shading.png");
+    MatrixXd C = MatrixXd::Zero(800, 800); // Store the color
+    MatrixXd A = MatrixXd::Zero(800, 800); // Store the alpha mask
+
+    const Vector3d camera_origin(0, 0, 3);
+    const Vector3d camera_view_direction(0, 0, -1);
+
+    // The camera is perspective, pointing in the direction -z and covering the unit square (-1,1) in x and y
+    const Vector3d image_origin(-1, 1, 1);
+    const Vector3d x_displacement(2.0 / A.cols(), 0, 0);
+    const Vector3d y_displacement(0, -2.0 / A.rows(), 0);
+
+    //Sphere setup
+    const Vector3d sphere_center(0, 0, 0);
+    const double sphere_radius = 0.9;
+
+    //material params
+    const Vector3d diffuse_color(1, 0, 1);
+    const double specular_exponent = 100;
+    const Vector3d specular_color(0., 0, 1);
+
+    // Single light source
+    const Vector3d light_position(-1, 1, 1);
+    double ambient = 0.1;
+
+    for (unsigned i = 0; i < C.cols(); ++i)
+    {
+        for (unsigned j = 0; j < C.rows(); ++j)
+        {
+            const Vector3d pixel_center = image_origin + double(i) * x_displacement + double(j) * y_displacement;
+
+            // Prepare the ray
+            const Vector3d ray_origin = pixel_center;
+            const Vector3d ray_direction = camera_view_direction;
+
+            // Intersect with the sphere
+            // NOTE: this is a special case of a sphere centered in the origin and for orthographic rays aligned with the z axis
+            Vector2d ray_on_xy(ray_origin(0), ray_origin(1));
+            const double sphere_radius = 0.9;
+
+            if (ray_on_xy.norm() < sphere_radius)
+            {
+                // The ray hit the sphere, compute the exact intersection point
+                Vector3d ray_intersection(
+                    ray_on_xy(0), ray_on_xy(1),
+                    sqrt(sphere_radius * sphere_radius - ray_on_xy.squaredNorm()));
+
+                // Compute normal at the intersection point
+                Vector3d ray_normal = ray_intersection.normalized();
+
+                // TODO: Add shading parameter here
+                const double diffuse = (light_position - ray_intersection).normalized().dot(ray_normal);
+                const double specular = (light_position - ray_intersection).normalized().dot(ray_normal);
+
+                // Simple diffuse model
+                C(i, j) = ambient + diffuse + specular;
+
+                // Clamp to zero
+                C(i, j) = std::max(C(i, j), 0.);
+
+                // Disable the alpha mask for this pixel
+                A(i, j) = 1;
+            }
+        }
+    }
+
+    // Save to png
+    write_matrix_to_png(C, C, C, A, filename);
+}
+
+int main()
+{
+    raytrace_sphere();
+    raytrace_parallelogram();
+    raytrace_perspective();
+    raytrace_shading();
+
+    return 0;
 }
